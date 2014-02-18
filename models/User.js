772c9bc73c03e11071e23fda706d76d263747f73
davidwang2007 <davidwang2007@gmail.com>
2014-02-18 13:31:18 -0500

    add User model

---
+++ models/User.js
@@ -0,0 +1,22 @@
+/**
+ * User model
+ * @author David Wang
+ * @email davidwang2006@outlook.com
+ */
+'use strict';
+var mongoose = require('mongoose');
+
+var userSchema = mongoose.schema({
+	username: String,
+	password: String,
+	age: {type: Number, default:18},
+	email: String,
+	cellphone: String
+});
+
+userSchema.methods.whoAmI = function(){
+	var greeting = 'Hello! My name is ' + this.username;
+	console.log(greeting);
+};
+
+module.exports = mongoose.model('User',userSchema);
